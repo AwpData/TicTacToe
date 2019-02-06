2cb33b2123e71c23c0149b90f4261523cba93726
AwpData <awpdata@gmail.com>
2019-02-06 08:59:09 -0800

    Update TicTacToe_Variables.java

--- TicTacToe_Variables.java
+++ TicTacToe_Variables.java
@@ -1,1 +1,11 @@
+public class TicTacToe_Variables {
+		public int boardSize; // how many rows the board will have
+		public int coordinates; // will ask user for coordinates and will be parsed into x and y
+		public int input; // for scanner input
+		public int x; // x-coordinate variable
+		public int y; // y-coordinate variable
+		public int player1; // points player 1 has
+		public int player2; // points player 2 has
+	}
+
 
