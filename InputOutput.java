c0fe1e50777a4bd595f323f0c92282c18a8503e0
username2112 <36423664+username2112@users.noreply.github.com>
2019-02-06 09:11:34 -0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,1 +1,24 @@
+package tictac;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//get input
+		Scanner input = new Scanner(System.in);
+		String full = input.nextLine()  + ";";
+		String xStr = full.substring(0, full.indexOf(",")); //split user input line into 2 variables (var 1 str)
+		String yStr = full.substring(full.indexOf(","), full.indexOf(";")); //split user input line into 2 variables (var 2 str)
+		int X = Integer.parseInt(xStr); //parse var 1
+		int Y = Integer.parseInt(yStr);	//parse var 2
+		
+		
+		
+	}
+
+}
 
