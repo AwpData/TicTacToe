30746e268f67d1c20e0b5347d9f1d7f1519f3d73
username2112 <36423664+username2112@users.noreply.github.com>
2019-02-08 07:53:16 -0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class InputOutput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		
 		//get input
+		Boolean Compat;
 		Scanner input = new Scanner(System.in);
 		String full = input.nextLine()  + ";";
-		String xStr = full.substring(0, full.indexOf(",")); //split user input line into 2 variables (var 1 str)
-		String yStr = full.substring(full.indexOf(","), full.indexOf(";")); //split user input line into 2 variables (var 2 str)
-		int X = Integer.parseInt(xStr); //parse var 1
-		int Y = Integer.parseInt(yStr);	//parse var 2
 		
+		String xStr = full.substring(0, full.indexOf(",")); //split user input line into 2 variables (var 1 str)
+		String yStr = full.substring(full.indexOf(",") + 1, full.indexOf(";")); //split user input line into 2 variables (var 2 str)
+		
+		int X = Integer.parseInt(xStr); //parse var 1
+		int Y = Integer.parseInt(yStr);	//parse var 2 (finds what row)
+		
+		//check for error
+		if(board[X][Y].containes("O") || board[X][Y].containes("X")) {
+			Compat = false;
+		} else {
+			Compat = true;
+		}
 		
 		
 	}
