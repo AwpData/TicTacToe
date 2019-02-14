fc68c52351ca2ee64591ae7e15a68cdb78b7909d
AwpData <awpdata@gmail.com>
2019-02-14 09:33:00 -0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,31 +1,74 @@
-package tictac;
+import java.util.*;
 
-import java.util.Scanner;
-
-public class InputOutput {
-
+//Don't choose an occupied space or you miss your turn 
+public class TicTacToe {
 	public static void main(String[] args) {
-		
-		//get input
-		Boolean Compat;
+		boolean player1turn = true; // if player 1 can go
+		boolean player2turn = false; // if player 2 can go
+		boolean win = false; //determines if someone won
+		boolean tryagain = false;
 		Scanner input = new Scanner(System.in);
-		String full = input.nextLine()  + ";";
-		
-		String xStr = full.substring(0, full.indexOf(",")); //split user input line into 2 variables (var 1 str)
-		String yStr = full.substring(full.indexOf(",") + 1, full.indexOf(";")); //split user input line into 2 variables (var 2 str)
-		
-		int X = Integer.parseInt(xStr); //parse var 1
-		int Y = Integer.parseInt(yStr);	//parse var 2 (finds what row)
-		
-		//check for error
-		if(board[X][Y].containes("O") || board[X][Y].containes("X")) {
-			Compat = false;
-		} else {
-			Compat = true;
+		int boardsize = 3; //default boardsize (cannot change as it is not supported)
+		int num = 0;
+		String[] board = new String[boardsize * 2]; //create new board
+		String[] editedboard = board; //create a copy of the board to store values & to check for wins
+		String symbol = ""; //symbol X or O depending on player
+		String currentplayer = ""; //who the current player is 
+		for (int i = 0; i <= boardsize * 2 - 1; i += 2) { // base board
+			board[i] = "+-+-+-+";
+			board[i + 1] = "| | | |";
 		}
-		
-		
+		for(int i = 0; i <= boardsize * 2 - 1; i++) {
+			System.out.println(board[i]);
+		}
+		System.out.println("+-+-+-+");
+		for (int i = 1; i < editedboard.length; i += 2) { // board without the borders (For filling in)
+			editedboard[i - 1] = board[i];
+		}
+		while (!win) { // start of user input
+			System.out.println();
+			if (player1turn) { // test section
+				System.out.println("Player 1 your move: ");
+				symbol = "X";
+				currentplayer = "Player 1";
+			} else if (player2turn) {
+				System.out.println("Player 2 your move: ");
+				symbol = "O"; // test section end
+				currentplayer = "Player 2";
+			}
+			String coordinates = input.nextLine();
+			String xStr = coordinates.substring(0, coordinates.indexOf(",")); // gets x input
+			String yStr = coordinates.substring(coordinates.indexOf(",") + 1); // gets y input
+			int x = Integer.parseInt(xStr); // parse var 1 (finds what row)
+			int y = Integer.parseInt(yStr); // parse var 2 (finds what column)
+			tryagain = placingAssignment(x, y, editedboard, symbol); //places X and Y on coordinates
+			
+			for (int i = 0; i < editedboard.length - 3; i++) { // Prints out board each turn
+				System.out.println("+-+-+-+");
+				System.out.println(editedboard[i].toString());
+			}
+			System.out.println("+-+-+-+");
+			if (tryagain) { //Player retries 
+				continue;
+			}
+			if (checkForWin(editedboard)) { //Checks win methods
+				System.out.println(currentplayer + " wins!");
+				break;
+			} 
+			num++; //Adds +1 for each space filled (for ties) 
+			if (num == 9) { //9 = All spaces filled 
+				System.out.println("Tie game!");
+				break;
+			}
+			if (!player1turn && !tryagain) { //Changes player to 1
+				player1turn = true;
+				player2turn = false;
+				tryagain = false;
+			} else if (!player2turn && !tryagain) { // Changes player to 2
+				player2turn = true;
+				player1turn = false;
+				tryagain = false;
+			}
+		}
 	}
-
 }
-
